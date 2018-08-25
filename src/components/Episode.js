bedd35612969f15e3f297a45bce21e6c198b2daf
Sergey Kirienko <skirienko@gmail.com>
2018-08-25 17:25:59 +0300

    some changes

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -18,7 +18,7 @@ const Episode = (props) => {
             null
         }
         {props.episode.video ?
-            <div className="stretch"><div style={{padding:'56.25% 0 0 0',position:'relative'}}><iframe src={'https://player.vimeo.com/video/'+props.episode.video+'?byline=0&portrait=0'} style={{position:'absolute',top:0,left:0,width:'100%',height:'100%'}} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script></div>
+            <div className="stretch"><div style={{padding:'56.25% 0 0 0', position:'relative'}}><iframe src={'https://player.vimeo.com/video/'+props.episode.video+'?byline=0&portrait=0'} style={{position:'absolute',top:0,left:0,width:'100%',height:'100%'}} frameBorder="0" allowFullScreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script></div>
             :
             null
         }
