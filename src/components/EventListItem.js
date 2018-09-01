3a0fdf2a1015b96ad46c52ed3a245bd297c802c3
Sergey Kirienko <skirienko@fog.home.skirienko.info>
2018-09-02 00:34:45 +0300

    progressive image loading on main page

--- src/components/EventListItem.js
+++ src/components/EventListItem.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const EventListItem = (props) => {
-    console.log(props)
-    return (<li className="event-item">
-        <NavLink to={"/"+props.city+"/"+props.id}>
-            <img src={["/data", props.id, props.img].join('/')} alt="" width="240" height="160"/>
-            <div className="event-title">{props.title}</div>
-        </NavLink>
-        </li>);
+class EventListItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            opacity: 0
+        }
+    }
+
+    handleOnLoad() {
+        this.setState({opacity: 1});
+    }
+    
+    render() {
+        const link = "/"+this.props.city+"/"+this.props.id;
+        const src = ["/data", this.props.id, this.props.img].join('/');
+
+        return (<li className="event-item">
+            <NavLink to={link}>
+                <div className="progressive-img" style={{backgroundImage: `url(${this.props.thumb})`}}>
+                    <img src={src} alt="" width="240" height="160"
+                        style={{opacity:this.state.opacity}}
+                        onLoad={this.handleOnLoad.bind(this)}/></div>
+                <div className="event-title">{this.props.title}</div>
+            </NavLink>
+            </li>);
+    }
 }
 
 export default EventListItem;
