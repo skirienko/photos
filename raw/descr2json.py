409ca9ee8247b83575ceb1218df9d751de444c7e
Sergey Kirienko <skirienko@gmail.com>
2019-08-18 21:52:44 +0300

    Small update for Kaliningrad

--- raw/descr2json.py
+++ raw/descr2json.py
@@ -7,8 +7,8 @@ import os.path
 import cchardet as chardet
 
 # date = '2008-05-01'
-# date = '2008-05-02'
-date = '2009-07-01'
+date = '2008-05-02'
+# date = '2009-07-01'
 # date = '2014-06-11'
 # date = '2015-06-11'
 # date = '2015-06-20'
