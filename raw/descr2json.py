6cda78c2b386af68ea2ebb77d3a2904d6c950c71
Sergey Kirienko <skirienko@gmail.com>
2019-08-22 23:37:41 +0300

    Novgorod text update

--- raw/descr2json.py
+++ raw/descr2json.py
@@ -7,11 +7,11 @@ import os.path
 import cchardet as chardet
 
 # date = '2008-05-01'
-date = '2008-05-02'
+# date = '2008-05-02'
 # date = '2009-07-01'
 # date = '2014-06-11'
 # date = '2015-06-11'
-# date = '2015-06-20'
+date = '2015-06-20'
 # date = '2016-07-01'
 # date = '2018-09-10'
 
