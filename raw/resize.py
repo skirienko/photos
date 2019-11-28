a281b7d6a1186f36b1c64321bbe8f39cfbbb5b08
Sergey Kirienko <skirienko@gmail.com>
2019-11-29 00:30:21 +0200

    make resize.py multidirectorial

--- raw/resize.py
+++ raw/resize.py
@@ -4,25 +4,26 @@ import re
 from shutil import copy2
 import piexif
 
-# date = '2008-05-01'
-# date = '2008-05-02'
-# date = '2009-07-01'
-# date = '2014-06-11'
-# date = '2015-06-11'
-# date = '2015-06-20'
-# date = '2016-07-01'
-# date = '2017-07-11'
-# date = '2018-09-10'
-date = '2018-09-11'
-# date = '2018-09-12'
+dates = (
+    '2008-05-01',
+    '2008-05-02',
+    '2009-07-01',
+    '2014-06-11',
+    '2015-06-11',
+    '2015-06-20',
+    '2016-07-01',
+    '2017-07-11',
+    '2018-09-10',
+    '2018-09-11',
+    '2018-09-12',
+)
 
-dirname = "%s/orig" % date
-outdir = "../public/data/%s" % date
 MAX_SIDE = 1200
 PANO_MIN_SIZE = 600
 quality = 90
 
 watermarks = {}
+
 
 def generate_watermark(text):
 
@@ -85,65 +86,65 @@ def repair_exif(exif):
                 exif['GPS'][broken_field] = tuple(map(int, value.decode().split('/')))
 
 
-print("Started")
+def resize(orig_path, new_path):
+
+    with Image.open(orig_path, 'r') as img:
+        if img:
+            # print(img.size)
+            if os.path.exists(new_path):
+                if os.path.getmtime(new_path) >= os.path.getmtime(orig_path):
+                    # print("already exists, origin hasn't changed")
+                    return
+            byte_exif = b''
+            if 'exif' in img.info:
+                byte_exif = img.info['exif']
+                exif = piexif.load(byte_exif)
+                repair_exif(exif)
+
+                if exif and byte_exif:
+                    if piexif.ImageIFD.Orientation in exif['0th']:
+                        orientation = exif['0th'][piexif.ImageIFD.Orientation]
+                        if orientation == 6:
+                            print('Rotating 270')
+                            img = img.transpose(Image.ROTATE_270)
+                            exif['0th'][piexif.ImageIFD.Orientation] = 1
+                        elif orientation == 8:
+                            print('Rotating 90')
+                            img = img.transpose(Image.ROTATE_90)
+                            exif['0th'][piexif.ImageIFD.Orientation] = 1
+                    
+                    del exif['thumbnail']
+                    byte_exif = piexif.dump(exif)
+
+            if img.size[0] > MAX_SIDE or img.size[1] > MAX_SIDE:
+                print("Need to resize")
+                print(limit_size(img.size))
+                resized_img = img.resize(limit_size(img.size), Image.ANTIALIAS)
+                resized_img = add_watermark(resized_img).convert('RGB')
+                resized_img.save(new_path, 'JPEG', quality=quality, exif=byte_exif)
+            else:
+                print("Copy intact")
+                copy2(orig_path, new_path)
+
+
 rxPhoto = re.compile(r'[a-z_]+\d+[a-z_]*\.jpg', re.I) 
-# rxPhoto = re.compile(r'[a-z_]+1498[a-z_]*\.jpg', re.I)
 
-if not os.path.exists(outdir):
-    os.makedirs(outdir)
+for date in dates:
+    indir = "%s/orig" % date
+    outdir = "../public/data/%s" % date
 
-missed = [
-#     'IMG_1330.JPG',
-#     'IMG_1333.JPG',
-]
+    if not os.path.exists(indir):
+        print("Nothing to resize")
+        continue
 
-ls = os.listdir(dirname)
-for filename in ls:
-    if rxPhoto.match(filename):
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-        # processing missed photos only, comment following if you need to process all
-        if len(missed) > 0 and filename not in missed:
-            continue
+    ls = os.listdir(indir)
+    for filename in ls:
+        if rxPhoto.match(filename):
+            print(filename)
+            orig_path = '/'.join((indir, filename))
+            new_path = '/'.join((outdir, filename))
 
-        print(filename)
-        current_path = '/'.join((dirname, filename))
-        new_path = '/'.join((outdir, filename))
-
-        with Image.open(current_path, 'r') as img:
-            if img:
-                # print(img.size)
-                if os.path.exists(new_path):
-                    if os.path.getmtime(new_path) >= os.path.getmtime(current_path):
-                        # print("already exists, origin hasn't changed")
-                        continue
-                byte_exif = b''
-                if 'exif' in img.info:
-                    byte_exif = img.info['exif']
-                    exif = piexif.load(byte_exif)
-                    repair_exif(exif)
-
-                    if exif and byte_exif:
-                        if piexif.ImageIFD.Orientation in exif['0th']:
-                            orientation = exif['0th'][piexif.ImageIFD.Orientation]
-                            if orientation == 6:
-                                print('Rotating 270')
-                                img = img.transpose(Image.ROTATE_270)
-                                exif['0th'][piexif.ImageIFD.Orientation] = 1
-                            elif orientation == 8:
-                                print('Rotating 90')
-                                img = img.transpose(Image.ROTATE_90)
-                                exif['0th'][piexif.ImageIFD.Orientation] = 1
-                        
-                        del exif['thumbnail']
-                        byte_exif = piexif.dump(exif)
-
-                if img.size[0] > MAX_SIDE or img.size[1] > MAX_SIDE:
-                    print("Need to resize")
-                    print(limit_size(img.size))
-                    resized_img = img.resize(limit_size(img.size), Image.ANTIALIAS)
-                    resized_img = add_watermark(resized_img).convert('RGB')
-                    resized_img.save(new_path, 'JPEG', quality=quality, exif=byte_exif)
-                else:
-                    print("Copy intact")
-                    copy2(current_path, new_path)
-
+            resize(orig_path, new_path)
