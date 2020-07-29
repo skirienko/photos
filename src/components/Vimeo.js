5768721cdfbe7f774ca4f97e65da5ce8827fffc0
Sergey Kirienko <skirienko@gmail.com>
2020-07-30 01:00:19 +0300

    fix vimeo aspect ratio

--- src/components/Vimeo.js
+++ src/components/Vimeo.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 
 class Vimeo extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            params: {
+                aspect:56.25 // by default 16:9
+            },
+        }
+    }
+
+    async componentDidMount() {
+        const videoId = this.props.video;
+        let params = null;
+        try {
+            params = (await (await fetch(`https://vimeo.com/api/v2/video/${videoId}.json`)).json())[0];
+            if (params.height && params.width) {
+                params.aspect = params.height / params.width * 100;
+            }
+            this.setState({params: params});
+        }
+        catch(error) {
+            console.warn(`Could not get params for vimeo ${videoId}`);
+        }
+    }
+
     render() {
+        const aspect = this.state.params.aspect;
         return (
             <div className="stretch">
-                <div style={{padding:'56.25% 0 0 0', position:'relative'}}>
-                    <iframe src={'https://player.vimeo.com/video/'+this.props.video+'?byline=0&portrait=0'}
+                <div style={{padding:`${aspect}% 0 0 0`, position:'relative'}}>
+                    <iframe src={'https://player.vimeo.com/video/'+this.props.video+'?byline=0&portrait=0&title=0&dnt=1'}
                         title={this.props.descr}
                         style={{position:'absolute',top:0,left:0,width:'100%',height:'100%'}} frameBorder="0" allowFullScreen>
                     </iframe>
