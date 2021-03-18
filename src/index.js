77a2b441b487eec09e7ca60c604facff083e8cfc
Sergey Kirienko <skirienko@gmail.com>
2021-03-18 20:33:45 +0200

    update manifest, enable SW and bump version

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 const rootElement = document.getElementById('root');
 
 render(<App/>, rootElement);
 
-// registerServiceWorker();
+registerServiceWorker();
