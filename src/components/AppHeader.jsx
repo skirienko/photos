e080f743c04f64513afb6b56b036d78423284a61
Sergey Kirienko <skirienko@gmail.com>
2023-08-27 16:44:38 +0300

    In-header search

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,29 +1,45 @@
 import LinkOrSpan from './LinkOrSpan';
-
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function AppHeader({albums}) {
 
-    const links = [
-      {path: '/', children: "Фотографии"}
-    ];
-    const parts = location.pathname.split('/');
+    let location = useLocation();
 
-    if (parts && parts.length > 2 && parts[0]==='') {
-      const a = parts[1];
-      if (albums && a in albums) {        
-        links.push({ path: `/${a}`, children: albums[a] });
-      }
-      else if (parts[1]==='tags') {
-        links.push({ path: '/tags', children: "Теги" });
-      }
+    let links = getHeaderParts(location);
+
+    useEffect(() => {
+        links = getHeaderParts(location)
+    }, [location]);
+
+    function getHeaderParts(location) {
+        const links = [
+            {path: '/', children: "Фотографии"}
+        ];
+        const parts = location.pathname.split('/');
+
+        if (parts && parts.length > 2 && parts[0]==='') {
+          const a = parts[1];
+          if (albums && a in albums) {        
+            links.push({ path: `/${a}`, children: albums[a] });
+          }
+          else if (parts[1]==='tags') {
+            links.push({ path: '/tags', children: "Теги" });
+          }
+        }
+
+        links.forEach(l => {if (l.path===location.pathname) l.path = null; });
+        return links;  
     }
-    links.forEach(l => {if (l.path===location.pathname) l.path = null; });
 
     return (
       <header className="app__header">
         <h1 className="app__title">
           {links.map(l => <LinkOrSpan key={l.path} className="app__home" {...l}/>).reduce((a, b) => [a, ' › ', b])}
         </h1>
+        <form className="app__search" action="/search" >
+          <input type="search" name="q" placeholder="&#x2315;"/>
+        </form>
       </header>
     );
 }
