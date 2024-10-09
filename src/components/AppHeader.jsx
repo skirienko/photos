252aa927b5f76c6223bb3b08aa0a38d65b1712d5
Sergey Kirienko <skirienko@gmail.com>
2024-10-09 22:16:02 +0300

    small refactoring of header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,37 +2,41 @@ import LinkOrSpan from './LinkOrSpan';
 import { useLocation, useSearchParams, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
+function Loupe() {
+  return <svg viewBox="0 0 16 16" fill="none"><circle cx="6.5" cy="6.5" r="5.75"/><line x1="11" y1="11" x2="15" y2="15" strokeLinecap="round"/></svg>
+}
+
+function getHeaderParts(albums, location) {
+  const links = [
+      {path: '/', children: "Фотографии"}
+  ];
+  const parts = location.pathname.split('/');
+
+  if (parts && parts.length > 2 && parts[0]==='') {
+    const a = parts[1];
+    if (albums && a in albums) {        
+      links.push({ path: `/${a}`, children: albums[a] });
+    }
+    else if (parts[1]==='tags') {
+      links.push({ path: '/tags', children: "Теги" });
+    }
+  }
+
+  links.forEach(l => {if (l.path===location.pathname) l.path = null; });
+  return links;  
+}
+
 export default function AppHeader({albums}) {
 
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
     let location = useLocation();
 
-    let links = getHeaderParts(location);
+    let links = getHeaderParts(albums, location);
 
     useEffect(() => {
-        links = getHeaderParts(location)
+        links = getHeaderParts(albums, location)
     }, [location]);
-
-    function getHeaderParts(location) {
-        const links = [
-            {path: '/', children: "Фотографии"}
-        ];
-        const parts = location.pathname.split('/');
-
-        if (parts && parts.length > 2 && parts[0]==='') {
-          const a = parts[1];
-          if (albums && a in albums) {        
-            links.push({ path: `/${a}`, children: albums[a] });
-          }
-          else if (parts[1]==='tags') {
-            links.push({ path: '/tags', children: "Теги" });
-          }
-        }
-
-        links.forEach(l => {if (l.path===location.pathname) l.path = null; });
-        return links;  
-    }
 
     function handleSubmit(ev) {
       const inp = ev.target.q;
@@ -55,10 +59,7 @@ export default function AppHeader({albums}) {
         </h1>
         <form className="app__search" action="/search" onSubmit={handleSubmit}>
           <input type="search" className="app_search-input" name="q" placeholder=" " defaultValue={searchParams.get('q')}/>
-          <button className="app__search-button"><svg viewBox="0 0 16 16" fill="none">
-            <circle cx="6.5" cy="6.5" r="5.75"/>
-            <line x1="11" y1="11" x2="15" y2="15" strokeLinecap="round"/>
-            </svg></button>
+          <button className="app__search-button"><Loupe/></button>
         </form>
       </header>
     );
