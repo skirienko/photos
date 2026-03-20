9a6f99f103147b5b62fcc4e947bed06795524873
Sergey Kirienko <skirienko@gmail.com>
2026-03-20 22:22:07 +0200

    update version, configs, some content, tags and search

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +14,10 @@ export default defineConfig({
   },
   plugins: [
     react(), 
-    viteTsconfigPaths(),
   ],
+  /*
+  resolve: {
+    viteTsconfigPaths: true
+  }
+  */
 });
