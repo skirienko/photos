e4b0582ff4e2b2b3876a2478a0f3ba6c5753d63d
Sergey Kirienko <skirienko@gmail.com>
2026-06-23 22:17:11 +0300

    refactor resize script

--- raw/resize.py
+++ raw/resize.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 import os
 from subprocess import call
 import re
@@ -7,19 +7,15 @@ import piexif
 from pillow_heif import register_heif_opener
 
 from presets import albums
-from utils import rxPhoto, rxVideo
+from utils import rxPhoto, rxVideo, add_watermark, repair_exif, limit_size, MAX_SIDE
 
 rxWebp = re.compile(r'.*\.webp$', re.I)
 
-MAX_SIDE = 1200
-PANO_MIN_SIZE = 600
 quality = 90
 
 SAVE_JPEG = True
 SAVE_WEBP = True
 SKIP_VIDEO = True
-
-watermarks = {}
 
 # cmd_video_resize = './HandBrakeCLI.exe -i %s -o %s' # Windows
 cmd_video_resize = './HandBrakeCLI -i %s -o %s'  # macOS
@@ -33,66 +29,20 @@ albums = {
 register_heif_opener()
 
 
-def generate_watermark(text):
-    font_size_px = 20
+def resize_file(filename):
+    orig_path = '/'.join(('.', indir, filename))
+    new_path = '/'.join((outdir, filename))
 
-    if text in watermarks:
-        return watermarks[text]
+    if rxPhoto.match(filename):
+        print(filename)
+        resize_photo(orig_path, new_path)
 
-    font_size_pt = font_size_px // 4 * 3
-    s = (len(text) + 2) * font_size_px // 2
-    img = Image.new('RGBA', (s, s), (0, 0, 0, 0))
-    drawing = ImageDraw.Draw(img)
-    font = ImageFont.truetype(font='./fonts/PTSansBold.ttf', size=font_size_pt)
-
-    fill = (255, 255, 255, 80)
-    print("generate watermark '%s'" % text)
-
-    line_height = font_size_px + font_size_px // 4
-    drawing.text((font_size_px // 2, s - line_height), text, fill=fill, font=font)
-    img = img.rotate(90, resample=Image.BICUBIC).crop((s - line_height, 0, s, s))
-    watermarks[text] = img
-    return img
+    if rxVideo.match(filename) and not SKIP_VIDEO:
+        print(filename)
+        resize_video(orig_path, new_path)
 
 
-def add_watermark(img):
-    transparent = Image.new('RGBA', img.size, (0, 0, 0, 0))
-    transparent.paste(img)
-
-    watermark = generate_watermark('Sergey Kirienko')
-    # bottom right corner
-    position = (img.size[0] - watermark.size[0], img.size[1] - watermark.size[1])
-    transparent.paste(watermark, position, mask=watermark)
-    return transparent
-
-
-def limit_size(size):
-    if size[0] <= MAX_SIDE and size[1] <= MAX_SIDE:
-        return size
-    else:
-        aspect = float(size[0]) / float(size[1])
-        print("aspect: %f" % aspect)
-        if aspect >= 1:
-            if aspect > 2:
-                print("PANORAMA!")
-                return [int(round(PANO_MIN_SIZE * aspect)), PANO_MIN_SIZE]
-            else:
-                return [MAX_SIDE, int(round(MAX_SIDE / aspect))]
-        else:
-            return [int(round(MAX_SIDE * aspect)), MAX_SIDE]
-
-
-def repair_exif(exif):
-    if exif:
-        # this field should be Rational but iPhone claims it Ascii
-        broken_field = piexif.GPSIFD.GPSHPositioningError
-        if 'GPS' in exif:
-            if broken_field in exif['GPS'] and isinstance(exif['GPS'][broken_field], bytes):
-                value = exif['GPS'][broken_field]
-                exif['GPS'][broken_field] = tuple(map(int, value.decode().split('/')))
-
-
-def resize(orig_path, new_path):
+def resize_photo(orig_path, new_path):
     with Image.open(orig_path, 'r') as img:
         if img:
             # print(img.size)
@@ -183,16 +133,4 @@ for album, dates in albums.items():
 
         ls = os.listdir(indir)
         for filename in ls:
-            if rxPhoto.match(filename):
-                print(filename)
-                orig_path = '/'.join(('.', indir, filename))
-                new_path = '/'.join((outdir, filename))
-
-                resize(orig_path, new_path)
-
-            if rxVideo.match(filename) and not SKIP_VIDEO:
-                print(filename)
-                orig_path = '/'.join(('.', indir, filename))
-                new_path = '/'.join(('.', outdir, filename))
-
-                resize_video(orig_path, new_path)
+            resize_file(filename)
--- raw/utils.py
+++ raw/utils.py
@@ -1,9 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from os import path, listdir
 import io
 import base64
 import re
 import chardet
+import piexif
 
 rxDate = re.compile(r'\d{4}-\d\d-\d\d', re.I)
 rxPhoto = re.compile(r'[a-z_\-\d]+\.(jpe?g|png|gif|webp|heic)', re.I)
@@ -41,6 +42,25 @@ def get_aspect(fullpath):
     return aspect
 
 
+MAX_SIDE = 1200
+PANO_MIN_SIZE = 600
+
+def limit_size(size):
+    if size[0] <= MAX_SIDE and size[1] <= MAX_SIDE:
+        return size
+    else:
+        aspect = float(size[0]) / float(size[1])
+        print("aspect: %f" % aspect)
+        if aspect >= 1:
+            if aspect > 2:
+                print("PANORAMA!")
+                return [int(round(PANO_MIN_SIZE * aspect)), PANO_MIN_SIZE]
+            else:
+                return [MAX_SIDE, int(round(MAX_SIDE / aspect))]
+        else:
+            return [int(round(MAX_SIDE * aspect)), MAX_SIDE]
+
+
 def generate_thumb(fullname):
     thumb = None
     if path.isfile(fullname):
@@ -57,6 +77,49 @@ def generate_thumb(fullname):
     return thumb
 
 
+watermarks = {}
+
+def generate_watermark(text):
+    font_size_px = 20
+
+    if text in watermarks:
+        return watermarks[text]
+
+    font_size_pt = font_size_px // 4 * 3
+    s = (len(text) + 2) * font_size_px // 2
+    img = Image.new('RGBA', (s, s), (0, 0, 0, 0))
+    drawing = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font='./fonts/PTSansBold.ttf', size=font_size_pt)
+
+    fill = (255, 255, 255, 80)
+    print("generate watermark '%s'" % text)
+
+    line_height = font_size_px + font_size_px // 4
+    drawing.text((font_size_px // 2, s - line_height), text, fill=fill, font=font)
+    img = img.rotate(90, resample=Image.BICUBIC).crop((s - line_height, 0, s, s))
+    watermarks[text] = img
+    return img
+
+
+def add_watermark(img):
+    transparent = Image.new('RGBA', img.size, (0, 0, 0, 0))
+    transparent.paste(img)
+
+    watermark = generate_watermark('Sergey Kirienko')
+    # bottom right corner
+    position = (img.size[0] - watermark.size[0], img.size[1] - watermark.size[1])
+    transparent.paste(watermark, position, mask=watermark)
+    return transparent
+
+
+def repair_exif(exif):
+    if exif:
+        # this field should be Rational but iPhone claims it Ascii
+        broken_field = piexif.GPSIFD.GPSHPositioningError
+        if 'GPS' in exif:
+            if broken_field in exif['GPS'] and isinstance(exif['GPS'][broken_field], bytes):
+                value = exif['GPS'][broken_field]
+                exif['GPS'][broken_field] = tuple(map(int, value.decode().split('/')))
 
 
 def detect_encoding(filename):
